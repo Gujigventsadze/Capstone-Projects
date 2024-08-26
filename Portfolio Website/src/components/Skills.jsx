16ec118045332b6738ba0fb97da2be41f32cd4b5
GujiGventsadze <gventsadzeguji@gmail.com>
2024-08-26 23:17:37 +0400

    added skill section fully functional plus appearing animations on sections

--- Portfolio Website/src/components/Skills.jsx
+++ Portfolio Website/src/components/Skills.jsx
@@ -1,17 +1,84 @@
 import React, { useState } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { useInView } from 'react-intersection-observer';
 import cssImg from "../pics/css.png";
 import reactImg from "../pics/react.png";
 import postgresImg from "../pics/postgres.png";
 import nodejsImg from "../pics/nodejs.png";
 import javascriptImg from "../pics/javascript.png";
 import htmlImg from "../pics/html.png";
+import bootstrapImg from "../pics/bootstrap.png";
 import "../Skills.css";
+import { css } from '@emotion/react';
+
+
+
+
 
 export const Skills = () => {
+    const { ref, inView } = useInView({
+        triggerOnce: true, // Trigger the animation only once
+        threshold: 0.3, // Adjust this value based on when you want to trigger the animation
+      });
+
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     return (
-        <div>
-            Hello
+       <section id='skills' ref={ref} className={`welcome-section ${inView ? 'animate' : ''}`}>
+        <div className='line'></div>
+        <div className='skills-title'>
+            Below You Can See the Languages I know fluently
         </div>
+        <div className='carousel-container'>
+            <Carousel responsive={responsive}>
+                <div>
+                    <img src={htmlImg} alt='html icon' />
+                    <p>HTML</p>
+                </div>
+                <div>
+                    <img src={cssImg} alt='html icon' />
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <img src={javascriptImg} alt='html icon' />
+                    <p>JavaScript</p>
+                </div>
+                <div>
+                    <img src={reactImg} alt='html icon' />
+                    <p>ReactJS</p>
+                </div>
+                <div>
+                    <img src={nodejsImg} alt='html icon' />
+                    <p>NodeJS</p>
+                </div>
+                <div>
+                    <img src={postgresImg} alt='html icon' />
+                    <p>PostgreSQL</p>
+                </div>
+                <div>
+                    <img src={bootstrapImg} alt='html icon' />
+                    <p>Bootstrap</p>
+                </div>
+            </Carousel>
+        </div>
+       </section>
     )
 }
 
--- Portfolio Website/src/components/Welcome.jsx
+++ Portfolio Website/src/components/Welcome.jsx
@@ -1,8 +1,14 @@
 import {useRef, useEffect} from "react";
 import "../Welcome.css";
 import astroImg from "../astronait.png";
+import { useInView } from 'react-intersection-observer';
 
 export const Welcome = () => {
+    const { ref, inView } = useInView({
+        triggerOnce: true, // Trigger the animation only once
+        threshold: 0.5, // Adjust this value based on when you want to trigger the animation
+      });
+
     const imgRef = useRef(null);
 
     useEffect(() => {
@@ -13,11 +19,11 @@ export const Welcome = () => {
         }
     }, []);
     return (
-        <section id="home">
+        <section id="home"  ref={ref} className={`welcome-section ${inView ? 'animate' : ''}`}>
             <div className="welcome-container">
                 <div className="left-side">
                     <div className="content">
-                        <h1>Welcome to My Portfolio!</h1>
+                        <h1>Welcome to My Portfolio</h1>
                         <p>My Name is Elguja and I am a
                             Web Developer</p>
                     </div>
